64667321039bd3daf17d63e487b7048579e03e19
JOSHUA SAUNDERS <joshnsaunders@gmail.com>
2017-11-22 14:56:03 -0700

    refactor code lesson

--- routes/books.js
+++ routes/books.js
@@ -2,31 +2,51 @@ const knex = require('../lib/knex')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  knex('books')
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt')
-  .orderBy('title', 'ASC')
-  .then(data => res.send(data))
-  .catch(err => res.status(500).send(err))
-})
-
-router.get('/:id', (req, res, next) => {
-  // make sure that id is a number,
-  // if it is not a number, send a 400 (Bad Request)
-  if (!req.params.id || Number.isNaN(req.params.id)) {
+function checkInput({id}) {
+  if (!id || Number.isNaN(id)) {
     throw new Error('Invalid ID')
   }
+  return id
+}
 
-  knex('books')
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt')
-  // .where('id', req.params.id)
-  .first()
+function getOne(id) {
+  return knex('books')
+    .where('id', id)
+    .first()
+}
+
+function update(book) {
+  return knex('books')
+    .update({
+      title: book.title,
+      author: book.author,
+      genre: book.genre,
+      description: book.description,
+      cover_url: book.coverUrl,
+    }, '*')
+    .where('id', book.id)
+}
+
+function getAll(){
+  return knex ('books')
+  .orderBy('title', 'ASC')
+}
+
+router.get('/:id', (req, res, next) => {
+  let id = checkInput(req.params)
+  getOne(id)
   .then((data) => {
     res.send(data)
   })
   .catch(err => {
     res.status(500).send(err)
   })
+})
+
+router.get('/', (req, res, next) => {
+  getAll()
+  .then(data => res.send(data))
+  .catch(err => res.status(500).send(err))
 })
 
 router.post('/', (req, res, next) => {
@@ -48,56 +68,39 @@ router.post('/', (req, res, next) => {
 })
 
 router.put('/:id', (req, res, next) => {
-  // Make sure that id is a number
-  if (!req.params.id || Number.isNaN(req.params.id)) {
-    throw new Error('Invalid ID')
-  }
-
-  knex('books')
-    .where('id', req.params.id)
-    .first()
-    .then(book => {
-      if (!book) {
-        return next()
-      }
-
-      return knex('books')
-        .update({
-          title: req.body.title,
-          author: req.body.author,
-          genre: req.body.genre,
-          description: req.body.description,
-          cover_url: req.body.coverUrl,
-        }, '*')
-        .where('id', req.params.id)
-    })
-    .then(books => {
-      if (!books) { return {message: 'nothing found for given id'} }
-      const newObj = {
-        id: books[0].id,
-        title: books[0].title,
-        author: books[0].author,
-        genre: books[0].genre,
-        description: books[0].description,
-        coverUrl: books[0].cover_url,
-      }
-      res.send(newObj)
-    })
-    .catch(err => {
-      next(err)
-    })
+  return Promise
+  .resolve(checkInput(req.params))
+  .then(getOne)
+  .then(book => {
+    if (!book || book.length === 0) {
+      return res.status(404).send({message: 'Not found'})
+    }
+    return req.body
+  })
+  .then(update)
+  .then(books => {
+    if (!books) { return {message: 'nothing found for given id'} }
+    const newObj = {
+      id: books[0].id,
+      title: books[0].title,
+      author: books[0].author,
+      genre: books[0].genre,
+      description: books[0].description,
+      coverUrl: books[0].cover_url,
+    }
+    res.send(newObj)
+  })
+  .catch(err => {
+    next(err)
+  })
 })
 
 router.delete('/:id', (req, res, next) => {
   let book = null
-
-  if (!req.params.id || Number.isNaN(req.params.id)) {
-    throw new Error('Invalid ID')
-  }
-
+  let id = checkInput(req.params)
   knex('books')
     .del()
-    .where('id', req.params.id)
+    .where('id', id)
     .catch(err => res.status(500).send(err))
     .then(data => {
       res.status(200).send({deleted: data})
